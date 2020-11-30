e7a4f01db6a1b73bfb90aacf9ab3dd2873f0269c
sbalkhair <s.balkhair@hotmail.com>
2020-11-30 23:21:28 +0300

    create new project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testtto
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
